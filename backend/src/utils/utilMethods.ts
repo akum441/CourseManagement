a307021814eeb0ac1ffa09b0807b5a4def45f227
Arun Kumar <arun111143@gmail.com>
2023-10-26 21:18:35 +0200

    Code cleaningin

--- backend/src/utils/utilMethods.ts
+++ backend/src/utils/utilMethods.ts
@@ -1,19 +1,19 @@
 
-export const validateRequest = (courses, res) => {
+export const validateRequest = (courses) => {
     for (const course of courses) {
         const { id, name, members, coach_id, description } = course;
         if (!id || !name || !members || !coach_id || !description) {
-            return res.status(400).send("One or more course objects are missing required keys");
+            throw new Error("One or more course objects are missing required keys");
         }
     }
 }
 
-export const validateMembers = (courses, res) => {
+export const validateMembers = (courses) => {
     for (const course of courses) {
         let members:any =course.members;
     if (typeof members === "string") {
-        return res.status(400).send("Please supply members as an array");
+        throw new Error("Please supply members as an array");
       } else if (members instanceof Array && members.length==0) {
-        return res.status(400).send("Empty members");
+        throw new Error("Empty members");
       }}
 }
