50f5267962519d577bd58be1e3044cea5da8bbc9
Arun Kumar <arun111143@gmail.com>
2023-10-28 02:53:52 +0200

    Removed db pswd and pushed secret to kbtl

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '$Ystem54AIL',
-  database: 'CourseManagement',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'CourseManagement',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
